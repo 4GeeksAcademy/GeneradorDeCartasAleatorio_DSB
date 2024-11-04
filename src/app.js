60722932875d9e413ffe3953d95925feed4f2a49
DiegoSB9 <diego_sb_95@hotmail.com>
2024-11-04 20:31:46 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let simbolCard = ["♦", "♥", "♠", "♣"];
+  let numCard = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function generateSimbol() {
+    let randomSimbol =
+      simbolCard[Math.floor(Math.random() * simbolCard.length)];
+    return randomSimbol;
+  }
+
+  function generateNum() {
+    let randomNum = numCard[Math.floor(Math.random() * numCard.length)];
+    return randomNum;
+  }
+
+  let generatedSimbol = generateSimbol();
+  document.getElementById("card-simbol").innerHTML = generatedSimbol;
+  document.getElementById("card-simbol-down").innerHTML = generatedSimbol;
+  document.getElementById("card-num").innerHTML = generateNum();
+
+  if (generatedSimbol === "♥" || generatedSimbol === "♦") {
+    document.getElementById("card-simbol").style.color = "red";
+    document.getElementById("card-simbol-down").style.color = "red";
+    document.getElementById("card-num").style.color = "red";
+  }
 };
